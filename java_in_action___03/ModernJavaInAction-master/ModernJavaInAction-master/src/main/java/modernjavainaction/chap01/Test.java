4e69fb8f3cc7d59e8364321041ac5bfa82742f14
abhishek-honey <abhishek.honey.git@gmail.com>
2020-05-17 20:55:54 +0530

    Change Log: Removed Spring in action, added .gitignore

--- java_in_action___03/ModernJavaInAction-master/ModernJavaInAction-master/src/main/java/modernjavainaction/chap01/Test.java
+++ java_in_action___03/ModernJavaInAction-master/ModernJavaInAction-master/src/main/java/modernjavainaction/chap01/Test.java
@@ -1,12 +1,35 @@
 package modernjavainaction.chap01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
 import modernjavainaction.chap01.FilteringApples.Apple;
 
 public class Test {
+	
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.twoSum(new int [] {1,5,2,3,9,7}, 6);
+	}
+    public int[] twoSum(int[] nums, int target) {
+        int [] ret = new int[2];
+        Arrays.sort(nums);
+        
+        System.out.println(Arrays.toString(nums));
+        for(int i = 0 ; i< nums.length ; i++){
+            for(int j = i+1 ; j < nums.length ; j++){
+                if(nums[i] + nums[j] == target){
+                    ret[0] = i;
+                    ret[1] = j;
+                    return ret;
+                }
+            }
+        }
+        return ret;
+    }
+    
 	public static List<Apple> filterGreenApples(List<Apple> inventory) {
 		List<Apple> result = new ArrayList<>();
 		for (Apple apple : inventory) {
