546a6925aa9638bb1ee9377812ae89fc527cb2a0
abhishek-honey <abhishek.honey.git@gmail.com>
2020-07-05 21:23:20 +0530

    Change log: 1. Finished Chapter 2 2. Started Chapter 3

--- java_threads_and_the_concurrency_utilities___05/eclipse/src/ch02/ThreadStopping/v4/ThreadStopping.java
+++ java_threads_and_the_concurrency_utilities___05/eclipse/src/ch02/ThreadStopping/v4/ThreadStopping.java
@@ -1,34 +1,26 @@
 package ch02.ThreadStopping.v4;
 
-public class ThreadStopping
-{
-   public static void main(String[] args)
-   {
-      class StoppableThread extends Thread
-      {
-         private volatile boolean stopped; // defaults to false
+public class ThreadStopping {
+	public static void main(String[] args) {
+		class StoppableThread extends Thread {
+			private volatile boolean stopped; // defaults to false
 
-         @Override
-         public void run()
-         {
-            while(!stopped)
-              System.out.println("running");
-         }
+			@Override
+			public void run() {
+				while (!stopped)
+					System.out.println("running");
+			}
 
-         void stopThread()
-         {
-            stopped = true;
-         }
-      }
-      StoppableThread thd = new StoppableThread();
-      thd.start();
-      try
-      {
-         Thread.sleep(1000); // sleep for 1 second
-      }
-      catch (InterruptedException ie)
-      {
-      }
-      thd.stopThread();
-   }
+			void stopThread() {
+				stopped = true;
+			}
+		}
+		StoppableThread thd = new StoppableThread();
+		thd.start();
+		try {
+			Thread.sleep(1000); // sleep for 1 second
+		} catch (InterruptedException ie) {
+		}
+		thd.stopThread();
+	}
 }
